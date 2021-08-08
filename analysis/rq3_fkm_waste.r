4f45f607aab9a13ae88b318ef690dcee84cacb6c
Marc Sallin <marc.sallin@outlook.com>
2021-08-08 15:16:26 +0200

    show r^2 and p of linear model instead of spearman correlation

--- analysis/rq3_fkm_waste.r
+++ analysis/rq3_fkm_waste.r
@@ -13,22 +13,32 @@ generate_regression_fkm_waste <- function(daily, fkm) {
 
     delay_columns <- daily[, c("id", get_waste_delay_data_column_names())]
     aggregated_delay <- aggregate(. ~ id, delay_columns, sum)
-    correlation_plots[["Delay"]] <- spearman_correlation(filtered_fkm, aggregated_delay, "Delay")
+    correlation_plots[["Delay"]] <- fit_lm(filtered_fkm, aggregated_delay, "Delay")
 
     time_spent_columns <- daily[, c("id", get_waste_time_spent_column_names())]
     aggregated_time_spent <- aggregate(. ~ id, time_spent_columns, sum)
-    correlation_plots[["Time Spent"]] <- spearman_correlation(filtered_fkm, aggregated_time_spent, "Time Spent")
+    correlation_plots[["Time Spent"]] <- fit_lm(filtered_fkm, aggregated_time_spent, "Time Spent")
 
     stress_column <- daily[, c("id", "stress")]
     aggregated_stress <- aggregate(. ~ id, stress_column, sum)
-    correlation_plots[["Stress"]] <- spearman_correlation(filtered_fkm, aggregated_stress, "Stress")
+    correlation_plots[["Stress"]] <- fit_lm(filtered_fkm, aggregated_stress, "Stress")
 
     customer_column <- daily[, c("id", "customer")]
     aggregated_customer <- aggregate(. ~ id, customer_column, sum)
-    correlation_plots[["Productivity"]] <- spearman_correlation(filtered_fkm, aggregated_customer, "Customer")
+    correlation_plots[["Productivity"]] <- fit_lm(filtered_fkm, aggregated_customer, "Customer")
 
     arranged_plot <- ggarrange(plotlist = correlation_plots)
     suppressMessages(ggsave(plot = arranged_plot, "results/rq_3_fkm_waste_correlation.pdf", device = "pdf"))
+}
+
+fit_lm <- function(fkm, waste, title) {
+    lm_data_frame <- prepare_data(fkm, waste)
+    plot <- ggscatter(lm_data_frame, x = "score", y = "total",
+        conf.int = TRUE, title = title, size=1) +
+        geom_smooth(formula = y ~ x, method = "lm", colour = "blue", size = 0.5) +
+        stat_cor(aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~")), label.x = 10) # Show R^2 of lm fit instead of rho
+        # stat_cor(method = "pearson", cor.coef.name = "rho") # Show rho of pearson correlation
+    return(plot)
 }
 
 prepare_data <- function(fkm, waste) {
@@ -48,12 +58,4 @@ prepare_data <- function(fkm, waste) {
     # We are only interested in the fkm score and the summed waste
     lm_data_frame <- data.frame(score, total)
     return(lm_data_frame)
-}
-spearman_correlation <- function(fkm, waste, title) {
-    lm_data_frame <- prepare_data(fkm, waste)
-    plot <- ggscatter(lm_data_frame, x = "score", y = "total",
-        conf.int = TRUE, title = title, size=1) +
-        geom_smooth(formula = y ~ x, method = "lm", colour = "blue", size = 0.5) +
-        stat_cor(method = "spearman", cor.coef.name = "rho")
-    return(plot)
 }
